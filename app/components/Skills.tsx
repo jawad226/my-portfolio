5d065eda173dff8038bbdc85b11bc58c6095fc04
Jawad Jameel <jawadc226@gmail.com>
2025-11-12 20:51:49 +0500

    final update

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -105,7 +105,7 @@ export default function Skills() {
           </p>
 
           <motion.a
-            href="/Jawad_Jameel.pd"
+            href="/Jawad_Jameel_Resume.pdf"
             download
             whileHover={{ scale: 1.05 }}
             whileTap={{ scale: 0.95 }}
